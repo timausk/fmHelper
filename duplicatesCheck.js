d6042259733989c10e7d283de72bd078b26bf979
timausk <timausk@googlemail.com>
2020-05-09 14:20:58 +0200

    [duplicatesCheck] Change - extract method from helper

--- duplicatesCheck.js
+++ duplicatesCheck.js
@@ -1,12 +1,38 @@
 const fm = require('./fmHelper');
 const config = require('./config');
 const print = require('./src/printer');
+const utils = require('./src/utils');
 
 const args = process.argv.slice(2);
 
 if (args.length === 2) {
   fm.buildPaths(config.fmUserDirs, args)
-    .then(paths => fm.duplicatesCheck(paths[0], paths[1]))
+    .then((paths) => {
+      let pathA = paths[0];
+      let pathB = paths[1];
+      
+      let filesFolderA = utils.getFilesFromFolder(pathA);
+      if (utils.isEmpty(filesFolderA)) {
+        print.info(`no files found in folder: ${pathA}`);
+        return;
+      }
+
+      let filesFolderB = utils.getFilesFromFolder(pathB);
+      if (utils.isEmpty(filesFolderB)) {
+        print.info(`no files found in folder: ${pathB}`);
+        return;
+      }
+
+      let duplicates = utils.findDuplicates(filesFolderA, filesFolderB);
+      if (utils.isEmpty(duplicates)) {
+        print.info('no duplicate files found');
+      } else {
+        print.ok('duplicate items found: ');
+        duplicates.forEach((item, index) => {
+          print.info(`${index}: ${item}`);
+        });
+      }
+    })
     .catch(err => print.error(err));
 } else {
   print.warning('you have to pass folder paths as arguments');
--- fmHelper.js
+++ fmHelper.js
@@ -1,32 +1,7 @@
-const print = require('./src/printer');
 const utils = require('./src/utils');
 const fs = require('fs');
 
 module.exports = {
-  duplicatesCheck: function (pathA, pathB) {
-    let filesFolderA = utils.getFilesFromFolder(pathA);
-    if (utils.isEmpty(filesFolderA)) {
-      print.info(`no files found in folder: ${pathA}`);
-      return;
-    }
-
-    let filesFolderB = utils.getFilesFromFolder(pathB);
-    if (utils.isEmpty(filesFolderB)) {
-      print.info(`no files found in folder: ${pathB}`);
-      return;
-    }
-
-    let duplicates = utils.findDuplicates(filesFolderA, filesFolderB);
-    if (utils.isEmpty(duplicates)) {
-      print.info('no duplicate files found');
-    } else {
-      print.ok('duplicate items found: ');
-      duplicates.forEach((item, index) => {
-        print.info(`${index}: ${item}`);
-      });
-    }
-  },
-
   getLocalConfig: function(path) {
     if (utils.isModuleAvailable(path)) {
       const config = require(path);
@@ -35,8 +10,8 @@ module.exports = {
   },
   
   /**
-   * @param {object} dirs
-   * @param {object[]} args
+   * @param {Object} dirs
+   * @param {string[]} args
    * @returns {Promise<string[], Error>}
    */
   buildPaths: function(dirs, args) {
