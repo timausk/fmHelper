0c9b5bf96100d89f172d4d40d50ea67816ed5203
timausk <timausk@googlemail.com>
2020-10-23 12:31:16 +0200

    [createConfigXml] change lib to xml-js

--- createConfigXml.js
+++ createConfigXml.js
@@ -4,7 +4,7 @@
  * @type {module:fs}
  */
 const fs = require('fs');
-const parser = require('xml2json');
+const convert = require('xml-js');
 const { parse } = require('path');
 const utils = require('./src/utils');
 const config = require('./config');
@@ -15,32 +15,36 @@ const ext = '.png';
 
 utils.getFiles(pathFaces, ext)
   .then((files) => {
-    fs.readFile( './templates/config.xml', function(err, data) {
+    fs.readFile( './templates/config.xml', 'utf8', function(err, data) {
       const pathTo = 'graphics/pictures/person/0/portrait';
-      const json = JSON.parse(parser.toJson(data, {reversible: true, trim: false}));
+      const obj = convert.xml2js(data);
+      let items = [];
 
-      // the node to append items (id="maps")
-      const node = json['record']['list'];
-      const items = [];
-
+      // create list of items to append to the file
       for (const file of files) {
         const id = parse(file).name;
         const item = {
-          'from': id,
-          'to': pathTo.replace('0', id)
+          'type': 'element',
+          'name': 'record',
+          'attributes': {
+            'from': id,
+            'to': pathTo.replace('0', id) 
+          }
         };
         items.push(item);
       }
-      node.record = items;
 
-      const xml = parser.toXml(JSON.stringify(json));
-
+      // append
+      obj.elements[0].elements[7].elements = items;
+      
+      const xml = convert.js2xml(obj, {spaces: 2});
+      
       fs.writeFile(targetFolder + '/config.xml', xml, function(err, data) {
         if (err) {
           console.log(err);
         }
         else {
-          console.log('created a config.xml for ' + files.length + 'ids');
+          console.log('created a config.xml for ' + files.length + ' ids');
         }
       });
     });
